691c4afaf42104a65a49c9aed4d0da2d26b95f79
Bogdan Bylica <bbylica40@gmail.com>
2024-06-12 22:46:31 +0200

    Add project files.

---
+++ 02_tydz__Typy_danych__V3/Exercise4.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_tydz__Typy_danych__V3
+{
+    public class Exercise4
+    {
+        public static void Start()
+        {
+            bool game = true;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Zadanie 4");
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("ESC or DELETE or END - powrót do menu głównego");
+                Console.WriteLine("(inny klawisz        - ponowny start programu)");
+
+                //if (Console.ReadKey().Key == ConsoleKey.Escape || Console.ReadKey().Key == ConsoleKey.Delete || Console.ReadKey().Key == ConsoleKey.End)
+                //{
+                //   game = false;
+                //}
+
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.Escape:
+                        game = false;
+                        break;
+                    case ConsoleKey.Delete:
+                        game = false;
+                        break;
+                    case ConsoleKey.End:
+                        game = false;
+                        break;
+                    default:
+                        break;
+                }
+            } while (game);
+        }
+    }
+}
---
+++ 02_tydz__Typy_danych__V3/Menu.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_tydz__Typy_danych__V3
+{
+    public class Menu
+    {
+        public string ArrowRight = "" + Strings.ChrW(8594);
+        public static void MainMenu()
+        {
+            //string prompt = "Użyj strzałki (" + Strings.ChrW(8593) + ", " + Strings.ChrW(8595) + "), Zatwierdź Enter (" + Strings.ChrW(8626) + ")";
+
+
+            //Console.SetWindowSize(95, 30);
+            //Console.SetBufferSize(120, 30);
+            //Console.BufferWidth = 95;
+            //Console.CursorVisible = false;
+            Console.Clear();
+            //Console.WriteLine("Szerokość okna: " + Console.BufferWidth);
+            //Console.WriteLine("Wysokość  okna: " + Console.BufferHeight);
+            //Console.ReadLine();
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Typy danych");
+            Console.WriteLine();
+            Console.WriteLine("  1. Deklaracja zmiennych dot. danych pracownika firmy");
+            Console.WriteLine("  2. Deklaracja 3 jednoliterowych zmiennych i wypisanie w odwrotnej kolejności ich deklaracji");
+            Console.WriteLine("  3. Obliczanie przekątnej prostokąta");
+            Console.WriteLine("  4. Tworzenie 2 zmiennych liczbowych i jednej tekstowej a następnie przypisanie im wartości");
+            Console.WriteLine("  5. Dane personalne użytkownika");
+            Console.WriteLine();
+            Console.WriteLine("Choose - Arrow Up (" + Strings.ChrW(8593) + "), Arrow Down (" + Strings.ChrW(8595) + ")");
+            Console.WriteLine("Accept - Enter (" + Strings.ChrW(8626) + ")");
+            Console.WriteLine("Finish - Esc or Delete or End");
+        }
+
+        public static void CursorPosition(int position, string txt)
+        {
+            Console.SetWindowSize(95, 30);
+            Console.SetBufferSize(95, 30);
+            Console.BufferWidth = 95;
+            Console.SetCursorPosition(0, position);
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write(txt);
+            Console.SetCursorPosition(0, 11);
+        }
+
+        public static void ExerciseNr(int nr)
+        {
+            switch (nr)
+            {
+                case 1:
+                    Exercise1.Start();
+                    break;
+                case 2:
+                    Exercise2.Start();
+                    break;
+                case 3:
+                    Exercise3.Start();
+                    break;
+                case 4:
+                    Exercise4.Start();
+                    break;
+                case 5:
+                    Exercise5.Start();
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+}
