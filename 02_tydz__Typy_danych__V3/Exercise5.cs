54b485d0eb5783078ac91ba1ee6e2fd1b4a3b458
Bogdan Bylica <bbylica40@gmail.com>
2024-06-14 13:19:44 +0200

    Suplement with content of task in each exercise

--- 02_tydz__Typy_danych__V3/Exercise5.cs
+++ 02_tydz__Typy_danych__V3/Exercise5.cs
@@ -19,7 +19,8 @@ namespace _02_tydz__Typy_danych__V3
 
                 // początek zadania
                 Console.WriteLine("Program prosi użytkownika o jego dane personalne tj.");
-                Console.WriteLine("imię, nazwisko,numer telefonu, adres email, wzrost, waga (np. 85,7), itp".);
+                Console.WriteLine("imię, nazwisko,numer telefonu, adres email, wzrost, waga (np. 85,7), itp.");
+                Console.WriteLine("imię, nazwisko,numer telefonu, adres email, wzrost, waga (np. 85,7), itp.");
                 Console.WriteLine("Odpowiedź jest przekonwertowana do odpowiedniego typu danych");
                 Console.WriteLine("z użyciem metody: typDanych.Parse(odpowiedźOdUżytkownika).");
                 Console.WriteLine();
@@ -46,6 +47,13 @@ namespace _02_tydz__Typy_danych__V3
                         break;
                 }
             } while (game);
+        }
+
+        private static void Announcement(string text)
+        {
+            Console.WriteLine();
+            Console.WriteLine(text + " , spróbuj powtórnie");
+            Console.WriteLine();
         }
     }
 }
