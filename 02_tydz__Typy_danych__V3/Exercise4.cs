d237e8cab5a719325be535c15f93cf0c916f7f93
Bogdan Bylica <bbylica40@gmail.com>
2024-06-22 18:03:20 +0200

    First Exercise

--- 02_tydz__Typy_danych__V3/Exercise4.cs
+++ 02_tydz__Typy_danych__V3/Exercise4.cs
@@ -17,13 +17,26 @@ namespace _02_tydz__Typy_danych__V3
                 Console.WriteLine("Zadanie 4");
                 Console.WriteLine();
 
-                // początek zadania
                 Console.WriteLine("Program tworzy 2 zmienne liczbowe oraz jedną tekstową, o poprawnych typach danych");
                 Console.WriteLine("a następnie przypisuje im następujące wartości:");
                 Console.WriteLine("a. 10");
                 Console.WriteLine("b. Szkoła Dotneta");
                 Console.WriteLine("c. 12,5");
                 Console.WriteLine();
+                Console.WriteLine("Przypisywanie wartości");
+                Console.WriteLine();
+
+                // początek zadania
+                int variable1 = 10;
+                string variable2 = "Szkoła DotNeta";
+                double variable3 = 12.5;
+
+                //Console.Clear();
+                Console.WriteLine("Przypisanie danych do trzech zmiennych");
+                Console.WriteLine();
+                Console.WriteLine($"zmienna 1: {variable1}");
+                Console.WriteLine($"zmienna 2: {variable2}");
+                Console.WriteLine($"zmienna 3: {variable3}");
                 // koniec zadania
 
                 Console.WriteLine();
@@ -46,13 +59,6 @@ namespace _02_tydz__Typy_danych__V3
                         break;
                 }
             } while (game);
-        }
-
-        private static void Announcement(string text)
-        {
-            Console.WriteLine();
-            Console.WriteLine(text + " , spróbuj powtórnie");
-            Console.WriteLine();
         }
     }
 }
